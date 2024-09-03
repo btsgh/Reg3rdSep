d009c94de861b4bd850def902ca8677d5d9c6770
Mohammad Shadab Buchh <shadab@appsmith.com>
2024-09-03 06:53:34 +0000

    commti

---
+++ pages/Page2/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,18 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+		// return moment().format("LLL")
+		// return _.chunk([1,2,3,4,5,67,43],3)
+		setInterval(() => {
+			showAlert("Hi", "error")
+		}, 2500, "Page2")
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+		clearInterval("Page2")
+	}
+}
